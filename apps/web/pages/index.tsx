2e6200e99237cd8b7d006c3757ddf7622278b353
Rom Shiri <roms12@gmail.com>
2022-04-11 12:19:24 +0200

    small change to text

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome web 👋
+              Welcome web sssss👋
             </h1>
           </div>
 
